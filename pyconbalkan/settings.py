80a96d014a8aa0eac6a8e3a02e55eb3bd12cd062
Kátia Nakamura <katia@kiwi.com>
2018-04-02 20:03:17 +0200

    settings: get SECRET_KEY from the env variables

--- pyconbalkan/settings.py
+++ pyconbalkan/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = os.getenv('SECRET_KEY', config('SECRET_KEY'))
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
