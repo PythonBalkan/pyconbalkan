30f5667c0241db2dcd0079d97ed374d9396a5457
Kátia Nakamura <katia@kiwi.com>
2018-04-11 21:58:51 +0200

    settings: create PDF_PATH variable

--- pyconbalkan/settings.py
+++ pyconbalkan/settings.py
@@ -119,4 +119,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'pyconbalkan/core/static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+PDF_PATH = 'pyconbalkan/core/static/pdf/'
