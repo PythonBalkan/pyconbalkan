3ef3c783c87413c3096de0ec1e98e6be96d7a45b
Ülgen Sarıkavak <ulgensrkvk@gmail.com>
2018-05-08 03:39:25 +0300

    Use Django2 style cleaner urls

--- pyconbalkan/urls.py
+++ pyconbalkan/urls.py
@@ -1,13 +1,13 @@
-from django.conf.urls import url, include
+from django.conf.urls import include
 from django.contrib import admin
+from django.urls import path
 from django.views.static import serve
 
-from pyconbalkan.core import views, routers
-from pyconbalkan.settings import PDF_ROOT
 from pyconbalkan.conference.api_urls import router as conference
-from pyconbalkan.speaker.api_urls import router as speaker
+from pyconbalkan.core import routers, views
 from pyconbalkan.organizers.api_urls import router as organizers
-
+from pyconbalkan.settings import PDF_ROOT
+from pyconbalkan.speaker.api_urls import router as speaker
 
 router = routers.DefaultRouter()
 router.extend(conference)
@@ -15,9 +15,9 @@ router.extend(speaker)
 router.extend(organizers)
 
 urlpatterns = [
-    url(r'^$', views.home, name='index'),
-    url(r'^organizers$', views.organizers, name='organizers'),
-    url(r'^coc$', serve, {'path': 'coc_pyconbalkan.pdf', 'document_root': PDF_ROOT}),
-    url(r'^admin/', admin.site.urls),
-    url(r'^api/', include(router.urls)), # API
+    path('', views.home, name='index'),
+    path('organizers', views.organizers, name='organizers'),
+    path('coc', serve, {'path': 'coc_pyconbalkan.pdf', 'document_root': PDF_ROOT}),
+    path('admin/', admin.site.urls),
+    path('api/', include(router.urls)), # API
 ]
