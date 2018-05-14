a482b6e20d48f231b7c24b879e71e22cfe115010
Viktor Kerkez <alefnula@gmail.com>
2018-05-14 16:40:06 +0200

    Limit the number of sponsors per sponsorship level

--- pyconbalkan/sponsors/models.py
+++ pyconbalkan/sponsors/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.core.exceptions import ValidationError
 from djchoices import DjangoChoices, ChoiceItem
 
 
@@ -10,8 +11,35 @@ class SponsorshipLevel(DjangoChoices):
     partner = ChoiceItem('Partner')
 
 
+LIMITS = {
+    SponsorshipLevel.keystone: 1,
+    SponsorshipLevel.platinum: 6,
+    SponsorshipLevel.gold: 10,
+    SponsorshipLevel.silver: 15,
+    SponsorshipLevel.partner: 10e6
+}
+
+
 class Sponsor(models.Model):
     name = models.CharField(max_length=256)
     description = models.TextField()
     level = models.CharField(max_length=16, choices=SponsorshipLevel.choices)
-    logo = models.ImageField(upload_to='static/img/sponsors')
+    logo = models.ImageField(
+        upload_to='static/img/sponsors', null=True, blank=True
+    )
+
+    def __str__(self):
+        return f'Sponsor [{self.name}]'
+
+    __repr__ = __str__
+
+    def save(self, force_insert=False, force_update=False, using=None,
+             update_fields=None):
+        count = Sponsor.objects.filter(level=self.level).count()
+        if count >= LIMITS[self.level]:
+            raise ValidationError(
+                f'Sponsorship level {self.level} allows only '
+                f'{LIMITS[self.level]} sponsors'
+            )
+        super().save(force_insert=force_insert, force_update=force_update,
+                     using=using, update_fields=update_fields)
