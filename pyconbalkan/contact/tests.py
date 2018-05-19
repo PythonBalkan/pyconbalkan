72b37221294b7eb97820a989b4659660ef8c547f
nadaj <nadajankovic2004@yahoo.com>
2018-05-19 16:59:59 +0200

    Tests added

--- pyconbalkan/contact/tests.py
+++ pyconbalkan/contact/tests.py
@@ -4,9 +4,14 @@ from pyconbalkan.contact.models import Contact
 
 
 class ContactTests(APITestCase):
-    def test_contact(self):
+    def test_contact_valid(self):
         number_existing_contacts = Contact.objects.count()
         data = {'name': 'client_name', 'company': '', 'email': 'client@client.com', 'message': 'Hi.....'}
         response = self.client.post('/api/contact/', data, format='json')
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
         self.assertEqual(Contact.objects.count(), number_existing_contacts + 1)
+
+    def test_contact_invalid(self):
+        data = {'name': 'client_name', 'company': '', 'email': 'clientlient.com', 'message': 'Hi.....'}
+        response = self.client.post('/api/contact/', data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
