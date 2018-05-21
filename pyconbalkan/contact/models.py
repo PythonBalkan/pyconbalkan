e79d5ad0f404c2004e8a77e6d5ac5b79bc9c5fde
nadaj <nadajankovic2004@yahoo.com>
2018-05-21 17:35:17 +0200

    Contact form generated

--- pyconbalkan/contact/models.py
+++ pyconbalkan/contact/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django import forms
+from django.forms import ModelForm
 
 
 class Contact(models.Model):
@@ -6,3 +8,19 @@ class Contact(models.Model):
     company = models.CharField(max_length=100, null=True, blank=True)
     email = models.EmailField()
     message = models.TextField()
+
+
+class ContactForm(ModelForm):
+    name = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Name', 'class': 'form-control'}),
+                           max_length=256, error_messages={'required': 'Please enter your name.'})
+    company = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Company', 'class': 'form-control'}),
+                              max_length=100, required=False)
+    email = forms.EmailField(widget=forms.TextInput(attrs={'placeholder': 'Email', 'class': 'form-control'}),
+                             error_messages={'required': 'Please enter a valid email address.',
+                                             'invalid': 'Please enter a valid email address.'})
+    message = forms.CharField(widget=forms.Textarea(attrs={'placeholder': 'Message', 'class': 'form-control'}),
+                              error_messages={'required': 'Please enter your message.'})
+
+    class Meta:
+        model = Contact
+        fields = '__all__'
