155cd4d7c869ffd00fe68e7984c9648e38b34b4f
Kátia Nakamura <katia@kiwi.com>
2018-05-23 22:11:56 +0200

    contact: fixes on contact form

--- pyconbalkan/contact/models.py
+++ pyconbalkan/contact/models.py
@@ -9,17 +9,23 @@ class Contact(models.Model):
     email = models.EmailField()
     message = models.TextField()
 
+    def __str__(self):
+        contact_str = '{} | {}'.format(self.name, self.email)
+        if self.company:
+            return '{} | {}'.format(contact_str, self.company)
+        return contact_str
+
 
 class ContactForm(ModelForm):
     name = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Name', 'class': 'form-control'}),
-                           max_length=256, error_messages={'required': 'Please enter your name.'})
+                           max_length=256, error_messages={'required': 'Please, enter your name.'}, label='')
     company = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Company', 'class': 'form-control'}),
-                              max_length=100, required=False)
+                              max_length=100, required=False, label='')
     email = forms.EmailField(widget=forms.TextInput(attrs={'placeholder': 'Email', 'class': 'form-control'}),
-                             error_messages={'required': 'Please enter a valid email address.',
-                                             'invalid': 'Please enter a valid email address.'})
+                             error_messages={'required': 'Please, enter a valid email address.',
+                                             'invalid': 'Please enter a valid email address.'}, label='')
     message = forms.CharField(widget=forms.Textarea(attrs={'placeholder': 'Message', 'class': 'form-control'}),
-                              error_messages={'required': 'Please enter your message.'})
+                              error_messages={'required': 'Please, enter your message.'}, label='')
 
     class Meta:
         model = Contact
--- pyconbalkan/contact/views.py
+++ pyconbalkan/contact/views.py
@@ -1,5 +1,7 @@
 from rest_framework import viewsets
 from rest_framework.permissions import AllowAny
+
+from pyconbalkan.conference.models import Conference
 from pyconbalkan.contact.serializers import ContactSerializer
 from pyconbalkan.contact.models import Contact
 from django.shortcuts import render
@@ -9,13 +11,21 @@ from .models import ContactForm
 class ContactViewSet(viewsets.ModelViewSet):
     queryset = Contact.objects.all()
     serializer_class = ContactSerializer
-    http_method_names = ['post']
     permission_classes = [AllowAny]
 
 
 def contact_view(request):
+    conference = Conference.objects.filter(active=True)
+    context = {
+        'conference': conference.first() if conference else None,
+    }
     if request.method == 'POST':
         form = ContactForm(request.POST)
+        if form.is_valid():
+            form.save()
+            context['success'] = 'Your message was saved successfully! '
+            form = ContactForm()
     else:
         form = ContactForm()
-    return render(request, 'contact.html', {'form': form})
+    context['form'] = form
+    return render(request, 'contact.html', context)
