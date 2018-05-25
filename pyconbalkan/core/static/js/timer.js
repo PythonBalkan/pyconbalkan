ef2514ef14819ae5920bae35bd2806ec8d030221
Kátia Nakamura <katia@kiwi.com>
2018-05-26 00:25:20 +0200

    conference: clear timer and add Buy Tickets button

--- pyconbalkan/core/static/js/timer.js
+++ pyconbalkan/core/static/js/timer.js
@@ -22,6 +22,10 @@ var x = setInterval(function() {
     // If the count down is over, write some text
     if (distance < 0) {
         clearInterval(x);
-        document.getElementById("demo").innerHTML = "EXPIRED";
+        // Output the result in an element with id="demo"
+        document.getElementById("days").innerHTML = "0";
+        document.getElementById("hours").innerHTML = "0:";
+        document.getElementById("minutes").innerHTML = "0:";
+        document.getElementById("seconds").innerHTML = "0";
     }
 }, 1000);
