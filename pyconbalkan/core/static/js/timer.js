6269f40587927ae13b8717cc1c97b10f61a1ff17
Konstantin Krbacevic <kole@osm.rs>
2018-06-05 12:58:30 +0200

    Redesign the website
    Mobile menu styling (WiP)
    
    Add assets
    
    Change event sidebar design
    
    Define html/body height so 100% height can be used on children
    
    Event sidebar media queries, logo positioning,
    
    Style sidebar line and spacings
    
    Menu resonsivness and mobile menu styling
    
    Page resonsivness fix
    
    Fix text alignment and spacings on mobile
    
    Add assets
    
    Fix and standardize spacings
    
    Add full width class for buttons
    
    Restyle speaker cards
    
    Add hover to logo, make event sidebar smaller on mobile devices
    
    Form styling
    
    Remove menu bottom margin
    
    Fix page margins
    
    Add margin bottom on titles
    
    Change title bottom margin back to 40px
    
    Add hr line as a separator
    
    Button styling
    
    Add event button link, fix style and spacing
    
    Fix form on mobile devices
    
    Add button wrapper
    
    Add target blank to coc link (temp)
    
    Add active class
    
    CFP input button fix
    
    Remove ::before menu button, add div .mobile-button
    
    Fix
    
    core: add menu.js
    
    Active menu fix
    
    core: toggle nav.menu instead of ul.menu__list
    
    core: update eventbrite link for tickets
    
    Menu active fix and styling

--- pyconbalkan/core/static/js/timer.js
+++ pyconbalkan/core/static/js/timer.js
@@ -1,30 +1,32 @@
 // Update the count down every 1 second
 var x = setInterval(function() {
 
-    // Get todays date and time
-    var now = new Date().getTime();
+    if (document.getElementById("days")) {
+        // Get today's date and time
+        var now = new Date().getTime();
 
-    // Find the distance between now an the count down date
-    var distance = countDownDate - now;
+        // Find the distance between now an the count down date
+        var distance = countDownDate - now;
 
-    // Time calculations for days, hours, minutes and seconds
-    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+        // Time calculations for days, hours, minutes and seconds
+        var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+        var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+        var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-    // Output the result in an element with id="demo"
-    document.getElementById("days").innerHTML = days;
-    document.getElementById("hours").innerHTML = hours;
-    document.getElementById("minutes").innerHTML = minutes;
-    document.getElementById("seconds").innerHTML = seconds;
+        // Output the result in an element with id="demo"
+        document.getElementById("days").innerHTML = days;
+        document.getElementById("hours").innerHTML = hours;
+        document.getElementById("minutes").innerHTML = minutes;
+        document.getElementById("seconds").innerHTML = seconds;
 
-    // If the count down is over, write some text
-    if (distance < 0) {
-        clearInterval(x);
-        document.getElementById("days").innerHTML = "0";
-        document.getElementById("hours").innerHTML = "0";
-        document.getElementById("minutes").innerHTML = "0";
-        document.getElementById("seconds").innerHTML = "0";
+        // If the count down is over, write some text
+        if (distance < 0) {
+            clearInterval(x);
+            document.getElementById("days").innerHTML = "0";
+            document.getElementById("hours").innerHTML = "0";
+            document.getElementById("minutes").innerHTML = "0";
+            document.getElementById("seconds").innerHTML = "0";
+        }
     }
 }, 1000);
