386414f706e79afdb32b542bc9b52301dc0a2fea
Bojan Jovanovic <bojan@kortechs.me>
2018-06-06 13:15:06 +0200

    added admin to about

--- generate-django.sh
+++ generate-django.sh
@@ -1,4 +1,6 @@
-#!/usr/bin/python
+#!/bin/bash
 
-import random
-print "".join([random.choice("abcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*(-_=+)") for i in range(50)])
+function django_secret() { python -c "import random,string;print('SECRET_KEY='+''.join([random.SystemRandom().choice(\"{}{}{}\".format(string.ascii_letters, string.digits, string.punctuation)) for i in range(63)]).replace('\\'','\\'\"\\'\"\\''))"; }
+echo "DEBUG=True" > .env
+django_secret >> .env
+
