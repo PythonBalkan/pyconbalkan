25f9360142141100cdf6ce1e134f771613c797c2
Kátia Nakamura <katia@kiwi.com>
2018-06-11 23:58:28 +0200

    organizers: add weight to volunteers and order by weight

--- pyconbalkan/organizers/views.py
+++ pyconbalkan/organizers/views.py
@@ -20,8 +20,8 @@ def organizer_view(request, slug):
 
 
 def organizers_listview(request):
-    volunteers = Volunteer.objects.filter(type=Volunteer.VOLUNTEER, active=True)
-    organizers = Volunteer.objects.filter(type=Volunteer.ORGANIZER, active=True)
+    volunteers = Volunteer.objects.filter(type=Volunteer.VOLUNTEER, active=True).order_by('weight')
+    organizers = Volunteer.objects.filter(type=Volunteer.ORGANIZER, active=True).order_by('weight')
     conference = Conference.objects.filter(active=True)
     context = {
         'volunteers': volunteers,
