a176af5fc8e1a3c58eb7326eb34234b2bf5d112d
Kátia Nakamura <katia@kiwi.com>
2018-06-29 17:25:26 +0200

    settings: add default="587" to EMAIL

--- pyconbalkan/settings.py
+++ pyconbalkan/settings.py
@@ -165,7 +165,7 @@ else:
     # Email
     EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
     EMAIL_HOST = config("EMAIL_HOST", "")
-    EMAIL_PORT = config("EMAIL_PORT", "", cast=int)
+    EMAIL_PORT = config("EMAIL_PORT", default="587", cast=int)
     EMAIL_HOST_USER = config("EMAIL_HOST_USER", "")
     EMAIL_HOST_PASSWORD = config("EMAIL_HOST_PASSWORD", "")
     EMAIL_USE_TLS = True
