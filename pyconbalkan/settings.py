66611f6fdcd204cc910a5c8f7105f86fd70becb7
Kátia Nakamura <katia@kiwi.com>
2018-06-29 17:13:02 +0200

    settings: cast EMAIL_PORT to int

--- pyconbalkan/settings.py
+++ pyconbalkan/settings.py
@@ -164,10 +164,10 @@ else:
     DEFAULT_FILE_STORAGE = "pyconbalkan.core.storage.S3Storage"
     # Email
     EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-    EMAIL_HOST = config("EMAIL_HOST", "")
-    EMAIL_PORT = config("EMAIL_HOST_USER", "")
-    EMAIL_HOST_USER = config("EMAIL_HOST_USER", "")
-    EMAIL_HOST_PASSWORD = config("EMAIL_HOST_PASSWORD", "")
+    EMAIL_HOST = config("EMAIL_HOST")
+    EMAIL_PORT = config("EMAIL_HOST_USER", cast=int)
+    EMAIL_HOST_USER = config("EMAIL_HOST_USER")
+    EMAIL_HOST_PASSWORD = config("EMAIL_HOST_PASSWORD")
     EMAIL_USE_TLS = True
     # Sentry config
     RAVEN_CONFIG = {
