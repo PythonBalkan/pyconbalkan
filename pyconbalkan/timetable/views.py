a04d01f8850cdef98d92b8ce8e43caf9f1abdc4b
Vishvajit Pathak <vishvajit@motoinsight.com>
2018-11-03 16:39:45 +0530

    Added Day1, Day2, Day3 text based on slot dates

--- pyconbalkan/timetable/views.py
+++ pyconbalkan/timetable/views.py
@@ -3,7 +3,13 @@ from rest_framework import viewsets
 
 from pyconbalkan.timetable.models import Presentation, Room, Slot, Timetable
 from pyconbalkan.timetable.serializers import TimetableSerializer
+from datetime import datetime
 
+DAYS = {
+    datetime.strptime('16Nov2018', '%d%b%Y').date(): 'Day 1',
+    datetime.strptime('17Nov2018', '%d%b%Y').date(): 'Day 2',
+    datetime.strptime('18Nov2018', '%d%b%Y').date(): 'Day 3',
+}
 
 class TimetableViewSet(viewsets.ModelViewSet):
     queryset = Timetable.objects.all()
@@ -11,7 +17,7 @@ class TimetableViewSet(viewsets.ModelViewSet):
 
 
 def timetable_view(request):
-    slots = Slot.objects.all()
+    slots = Slot.objects.all().order_by('from_date')
     rooms = Room.objects.all()
-    context = {'slots': slots, 'rooms': rooms}
+    context = {'slots': slots, 'rooms': rooms, 'DAYS': DAYS}
     return render(request, 'timetable.html', context)
