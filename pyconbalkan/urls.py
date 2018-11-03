d959b37cdfdc61fc47b2f3028a23a84f22a5efc1
Vishvajit Pathak <vishvajit@motoinsight.com>
2018-11-03 15:02:49 +0530

    url change for timetable_view

--- pyconbalkan/urls.py
+++ pyconbalkan/urls.py
@@ -3,14 +3,14 @@ from django.contrib import admin
 from django.urls import path
 from django.views.generic import TemplateView
 from django.views.static import serve
-from markdownx import urls as markdownx
 
+from markdownx import urls as markdownx
 from pyconbalkan.about.api_urls import router as about
 from pyconbalkan.about.views import about_view
 from pyconbalkan.cfp.api_urls import router as cfp
 from pyconbalkan.cfp.views import cfp_detail, cfp_list, cfp_view
 from pyconbalkan.coc.api_urls import router as coc
-from pyconbalkan.coc.views import response_guide, coc_view
+from pyconbalkan.coc.views import coc_view, response_guide
 from pyconbalkan.conference.api_urls import router as conference
 from pyconbalkan.contact.api_urls import router as contact
 from pyconbalkan.contact.views import contact_view
@@ -19,12 +19,15 @@ from pyconbalkan.info.views import info_view
 from pyconbalkan.news.api_urls import router as news
 from pyconbalkan.news.views import *
 from pyconbalkan.organizers.api_urls import router as organizers
-from pyconbalkan.organizers.views import organizer_view, volunteers_createview, organizers_list
+from pyconbalkan.organizers.views import (organizer_view, organizers_list,
+                                          volunteers_createview)
 from pyconbalkan.settings import PDF_ROOT
 from pyconbalkan.speaker.api_urls import router as speaker
 from pyconbalkan.speaker.views import *
 from pyconbalkan.sponsors.api_urls import router as sponsors
-from pyconbalkan.sponsors.views import sponsor_view, sponsoring_view, sponsors_view
+from pyconbalkan.sponsors.views import (sponsor_view, sponsoring_view,
+                                        sponsors_view)
+from pyconbalkan.timetable.views import timetable_view
 
 router = routers.DefaultRouter()
 router.extend(conference)
@@ -62,8 +65,8 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include(router.urls)),  # API
     path('markdownx/', include(markdownx)),
-    path('timetable/', serve, {'path': 'pycon-balkan-2018-timetable.pdf', 'document_root': PDF_ROOT}, name='timetable'),
-    # path('timetable/', timetable_view, name='timetable')
+    # path('timetable/', serve, {'path': 'pycon-balkan-2018-timetable.pdf', 'document_root': PDF_ROOT}, name='timetable'),
+    path('timetable/', timetable_view, name='timetable')
 ]
 
 # Add Django site authentication urls (for login, logout, password management)
