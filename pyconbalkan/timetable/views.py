5d062a5e93604e3e4bea627b8f70316dd54ee95b
Vishvajit Pathak <vishvajit@motoinsight.com>
2018-11-11 16:22:36 +0530

    PEP8 changes

--- pyconbalkan/timetable/views.py
+++ pyconbalkan/timetable/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from rest_framework import viewsets
 
-from pyconbalkan.timetable.models import Presentation, Room, Slot, Timetable
+from pyconbalkan.timetable.models import Room, Slot, Timetable
 from pyconbalkan.timetable.serializers import TimetableSerializer
 from datetime import datetime
 
@@ -10,6 +10,7 @@ DAYS = {
     datetime.strptime('17Nov2018', '%d%b%Y').date(): 'Day 2',
     datetime.strptime('18Nov2018', '%d%b%Y').date(): 'Day 3',
 }
+
 
 class TimetableViewSet(viewsets.ModelViewSet):
     queryset = Timetable.objects.all()
@@ -24,11 +25,10 @@ def timetable_view(request):
     slots_order_by_date = slots.order_by('from_date')
     for room in rooms:
         slots_by_rooms[room.name] = slots_order_by_date.filter(room=room)
-
     context = {
         'slots': slots_order_by_date,
         'slots_by_rooms': slots_by_rooms,
         'rooms': rooms,
-        'DAYS': DAYS,
+        'DAYS': DAYS
     }
     return render(request, 'timetable.html', context)
