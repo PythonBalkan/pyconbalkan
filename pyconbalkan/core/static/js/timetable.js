80c29c64cacac791d0316066827c63835bbc4637
Vishvajit Pathak <vishvajit@motoinsight.com>
2018-11-11 16:21:24 +0530

    Removed alert boxes

--- pyconbalkan/core/static/js/timetable.js
+++ pyconbalkan/core/static/js/timetable.js
@@ -1,9 +1,7 @@
 $(".btn-room").click(function() {
-    // alert('start');
     let roomNumber = this.innerHTML;
     $(".btn-room").removeClass('button--yellow');
     $(".presentations").addClass('hidden');
     $("." + roomNumber).removeClass('hidden');
     $(".btn-" + roomNumber).addClass('button--yellow');
-    // alert('end');
 });
