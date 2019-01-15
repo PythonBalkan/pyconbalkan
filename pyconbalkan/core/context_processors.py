af95c4d3dc375ce574d68ce67b64bdc38f79aecf
Bojan Jovanovic <bojan@kortechs.io>
2019-01-15 09:43:48 +0100

    Removed context_processor, and replaced fetching the conference from the request object

--- pyconbalkan/core/context_processors.py
+++
@@ -1,9 +0,0 @@
-from pyconbalkan.conference.models import Conference
-
-
-def conference(request):
-    conference = Conference.objects.filter(active=True)
-
-    return {
-        'conference': conference.first() if conference else None
-    }
