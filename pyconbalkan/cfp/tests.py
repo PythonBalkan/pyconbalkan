16af6871091f95f1faa1737f70a7868d7f555bf3
Bojan Jovanovic <bojan@kortechs.io>
2019-03-03 23:36:53 +0100

    Making Conference object for CFP's as required.
    + Black.

--- pyconbalkan/cfp/tests.py
+++ pyconbalkan/cfp/tests.py
@@ -1,11 +1,12 @@
 from django.contrib.auth.models import User
+from model_mommy import mommy
 from rest_framework import status
 from rest_framework.test import APITestCase
 from . import const
 
 from .models import Cfp
 
-CFP_URL = '/api/cfp/'
+CFP_URL = "/api/cfp/"
 
 
 class CfpAuthApiTest(APITestCase):
@@ -13,93 +14,144 @@ class CfpAuthApiTest(APITestCase):
 
     def setUp(self):
         # Create CFP
-        self.cfp = Cfp.objects.create(
-            name='test',
-            email='test@test.com',
-            title='Test',
-            duration='30min',
-            description='Test',
-            type=const.TALK
+        self.conference = mommy.make('conference.Conference')
+        self.cfp = mommy.make(
+            Cfp,
+            name="test",
+            email="test@test.com",
+            title="Test",
+            duration="30min",
+            description="Test",
+            conference=self.conference,
+            type=const.TALK,
         )
 
         # Create user
-        self.user = User.objects.create_superuser(username='test', password='1234', email='admin@test.com')
+        self.user = User.objects.create_superuser(
+            username="test", password="1234", email="admin@test.com"
+        )
 
     def test_list_returns_error_for_unauthenticated_user(self):
         """Test that unauthenticated user can't use CFP API endpoint."""
 
         result = self.client.get(CFP_URL)
-        self.assertEqual(status.HTTP_403_FORBIDDEN, result.status_code, 'Status code should be 403 for list.')
+        self.assertEqual(
+            status.HTTP_403_FORBIDDEN,
+            result.status_code,
+            "Status code should be 403 for list.",
+        )
 
     def test_create_returns_error_for_unauthenticated_user(self):
         """Test that unauthenticated user can't use CFP API endpoint."""
 
-        result = self.client.post(CFP_URL, {
-            'name': 'test1',
-            'email': 'test1@test.com',
-            'title': 'Test1',
-            'duration': 'Test1',
-            'description': 'Test',
-            'type': const.TALK,
-        })
-        self.assertEqual(status.HTTP_403_FORBIDDEN, result.status_code, 'Status code should be 403 for list.')
+        result = self.client.post(
+            CFP_URL,
+            {
+                "name": "test1",
+                "email": "test1@test.com",
+                "title": "Test1",
+                "duration": "Test1",
+                "description": "Test",
+                "type": const.TALK,
+            },
+        )
+        self.assertEqual(
+            status.HTTP_403_FORBIDDEN,
+            result.status_code,
+            "Status code should be 403 for list.",
+        )
 
     def test_details_returns_error_for_unauthenticated_user(self):
         """Test that unauthenticated user can't use CFP API endpoint."""
 
-        result = self.client.get(CFP_URL + '{}/'.format(self.cfp.id))
-        self.assertEqual(status.HTTP_403_FORBIDDEN, result.status_code, 'Status code should be 403 for list.')
+        result = self.client.get(CFP_URL + "{}/".format(self.cfp.id))
+        self.assertEqual(
+            status.HTTP_403_FORBIDDEN,
+            result.status_code,
+            "Status code should be 403 for list.",
+        )
 
     def test_update_returns_error_for_unauthenticated_user(self):
         """Test that unauthenticated user can't use CFP API endpoint."""
 
-        result = self.client.patch(CFP_URL + '{}/'.format(self.cfp.id))
-        self.assertEqual(status.HTTP_403_FORBIDDEN, result.status_code, 'Status code should be 403 for list.')
+        result = self.client.patch(CFP_URL + "{}/".format(self.cfp.id))
+        self.assertEqual(
+            status.HTTP_403_FORBIDDEN,
+            result.status_code,
+            "Status code should be 403 for list.",
+        )
 
     def test_delete_returns_error_for_unauthenticated_user(self):
         """Test that unauthenticated user can't use CFP API endpoint."""
 
-        result = self.client.delete(CFP_URL + '{}/'.format(self.cfp.id))
-        self.assertEqual(status.HTTP_403_FORBIDDEN, result.status_code, 'Status code should be 403 for list.')
+        result = self.client.delete(CFP_URL + "{}/".format(self.cfp.id))
+        self.assertEqual(
+            status.HTTP_403_FORBIDDEN,
+            result.status_code,
+            "Status code should be 403 for list.",
+        )
 
     def test_list_returns_success_for_unauthenticated_user(self):
         """Test that authenticated user can use CFP API endpoint."""
         self.client.force_login(self.user)
 
         result = self.client.get(CFP_URL)
-        self.assertEqual(status.HTTP_200_OK, result.status_code, 'Status code should be 200 for list.')
+        self.assertEqual(
+            status.HTTP_200_OK,
+            result.status_code,
+            "Status code should be 200 for list.",
+        )
 
     def test_create_returns_success_for_authenticated_user(self):
         """Test that authenticated user can use CFP API endpoint."""
         self.client.force_login(self.user)
 
-        result = self.client.post(CFP_URL, {
-            'name': 'test1',
-            'email': 'test1@test.com',
-            'title': 'Test1',
-            'duration': 'Test1',
-            'description': 'Test',
-            'type': const.TALK,
-        })
-        self.assertEqual(status.HTTP_201_CREATED, result.status_code, 'Status code should be 201 for create.')
+        result = self.client.post(
+            CFP_URL,
+            {
+                "name": "test1",
+                "email": "test1@test.com",
+                "title": "Test1",
+                "duration": "Test1",
+                "description": "Test",
+                "type": const.TALK,
+            },
+        )
+        self.assertEqual(
+            status.HTTP_201_CREATED,
+            result.status_code,
+            "Status code should be 201 for create.",
+        )
 
     def test_details_returns_success_for_authenticated_user(self):
         """Test that authenticated user can use CFP API endpoint."""
         self.client.force_login(self.user)
 
-        result = self.client.get(CFP_URL + '{}/'.format(self.cfp.id))
-        self.assertEqual(status.HTTP_200_OK, result.status_code, 'Status code should be 200 for details.')
+        result = self.client.get(CFP_URL + "{}/".format(self.cfp.id))
+        self.assertEqual(
+            status.HTTP_200_OK,
+            result.status_code,
+            "Status code should be 200 for details.",
+        )
 
     def test_update_returns_success_for_authenticated_user(self):
         """Test that authenticated user can use CFP API endpoint."""
         self.client.force_login(self.user)
 
-        result = self.client.patch(CFP_URL + '{}/'.format(self.cfp.id))
-        self.assertEqual(status.HTTP_200_OK, result.status_code, 'Status code should be 200 for update.')
+        result = self.client.patch(CFP_URL + "{}/".format(self.cfp.id))
+        self.assertEqual(
+            status.HTTP_200_OK,
+            result.status_code,
+            "Status code should be 200 for update.",
+        )
 
     def test_delete_returns_success_for_authenticated_user(self):
         """Test that authenticated user can use CFP API endpoint."""
         self.client.force_login(self.user)
 
-        result = self.client.delete(CFP_URL + '{}/'.format(self.cfp.id))
-        self.assertEqual(status.HTTP_204_NO_CONTENT, result.status_code, 'Status code should be 204 for delete.')
+        result = self.client.delete(CFP_URL + "{}/".format(self.cfp.id))
+        self.assertEqual(
+            status.HTTP_204_NO_CONTENT,
+            result.status_code,
+            "Status code should be 204 for delete.",
+        )
