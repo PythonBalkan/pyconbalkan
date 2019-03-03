c4c2617ff8c765cee1f72a4687e0a380e853a22f
Bojan Jovanovic <bojan@kortechs.io>
2019-03-03 17:47:57 +0100

    Speakers + Sponsors editing enabled.
    Signed-off-by: Bojan Jovanovic <bojan@kortechs.io>

--- pyconbalkan/sponsors/admin.py
+++ pyconbalkan/sponsors/admin.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
+
+from pyconbalkan.conference.abstractions import ConferenceAbstractAdmin
 from pyconbalkan.sponsors.models import Package, PackageItem, Sponsor, Sponsoring
 
-admin.site.register(Sponsor)
+
+admin.site.register(Sponsor, ConferenceAbstractAdmin)
 admin.site.register(Sponsoring)
 admin.site.register(Package)
 admin.site.register(PackageItem)
