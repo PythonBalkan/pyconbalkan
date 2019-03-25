e994468f6c4979198ea1b8c4d00b9f38f7727d98
Bojan Jovanovic <bojan@kortechs.io>
2019-03-25 17:28:38 +0100

    Admin for news omit ConferenceAbstractAdmin.

--- pyconbalkan/news/admin.py
+++ pyconbalkan/news/admin.py
@@ -1,11 +1,10 @@
 from django.contrib import admin
 from markdownx.admin import MarkdownxModelAdmin
 
-from pyconbalkan.conference.abstractions import ConferenceAbstractAdmin
 from pyconbalkan.news.models import Post
 
 
-class PostAdmin(ConferenceAbstractAdmin, MarkdownxModelAdmin):
+class PostAdmin(MarkdownxModelAdmin):
     class Meta:
         model = Post
 
