6262b954970f4b79a9ff978f9a614266b6c51563
Bojan Jovanovic <bojan@kortechs.io>
2019-08-27 14:10:46 +0200

    Added a filter by conference year in CFP Admin.

--- pyconbalkan/cfp/admin.py
+++ pyconbalkan/cfp/admin.py
@@ -1,17 +1,13 @@
 from django.contrib import admin
+
 from pyconbalkan.cfp.models import Cfp, CFPRating
 
 
+@admin.register(Cfp)
 class CfpAdmin(admin.ModelAdmin):
-    class Meta:
-        model = Cfp
+    list_filter = (
+        "conference__name",
+    )
 
 
-class CfpRatingAdmin(admin.ModelAdmin):
-    class Meta:
-        model = CFPRating
-
-
-admin.site.register(Cfp, CfpAdmin)
-admin.site.register(CFPRating, CfpAdmin)
-
+admin.site.register(CFPRating)
